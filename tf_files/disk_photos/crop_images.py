56593c706146fe653e90a089d41562cbc4f44b48
CorvetteCole <corvettecole@gmail.com>
2019-03-04 22:17:09 -0500

    add new images and scripts

---
+++ tf_files/disk_photos/crop_images.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from PIL import Image
+import os, sys
+
+directory = 'test'
+
+path = os.path.join(os.getcwd(), directory)
+dirs = os.listdir( path )
+desiredsize = 224
+
+def crop_image(input_image):
+	"""Pass input name image, output name image, x coordinate to start croping, y coordinate to start croping, width to crop, height to crop """
+	input_img = Image.open(input_image)
+	f,e = os.path.splitext(input_image)
+	start_x = (input_img.size[0] - desiredsize)/2
+	start_y = (input_img.size[1] - desiredsize)/2
+	box = (start_x, start_y, start_x + desiredsize, start_y + desiredsize)
+	output_img = input_img.crop(box)
+	output_img.save(f +".jpg", 'JPEG')
+
+def resize():
+	for item in dirs:
+		fullPath = os.path.join(path, item)
+		if os.path.isfile(fullPath):
+			crop_image(fullPath)
+		
+resize()
---
+++ tf_files/disk_photos/resize_images.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from PIL import Image
+import os, sys
+
+directory = 'wood'
+
+path = os.path.join(os.getcwd(), directory)
+dirs = os.listdir( path )
+basewidth = 224
+
+def resize():
+    for item in dirs:
+        fullPath = os.path.join(path, item)
+        if os.path.isfile(fullPath):
+            img = Image.open(fullPath)
+            f, e = os.path.splitext(fullPath)
+            wpercent = (basewidth/float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+            img.save(f +'.jpg', 'JPEG')
+
+resize()
