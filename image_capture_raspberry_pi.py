1fda10aba7215639f66d1b6912f374024785457c
CorvetteCole <corvettecole@gmail.com>
2019-03-05 13:23:19 -0500

    add more scripts

---
+++ image_capture_raspberry_pi.py
@@ -0,0 +1,18 @@
+import cv2
+import subprocess
+import os
+
+camera = cv2.VideoCapture(0)
+desiredsize = 224
+
+while True :
+	return_value, image = camera.read()
+	height, width, channels = image.shape 
+	start_x = int((width - desiredsize)/2)
+	start_y = int((height - desiredsize)/2)
+	crop_img = image[start_y:start_y+desiredsize, start_x:start_x+desiredsize]
+	cv2.imwrite('opencv.jpg', crop_img)
+	subprocess.call("python -m scripts.label_image --graph=tf_files/retrained_graph.pb --image=opencv.jpg", shell=True)
+	os.remove("opencv.jpg")
+	
+del(camera)
